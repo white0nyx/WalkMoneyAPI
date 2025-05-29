60bad31e74357b68a1bce4802488bae7668bf7de
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2025-05-29 18:40:39 +0300

    Добавлено поле type в Category

--- src/category/main/schemas.py
+++ src/category/main/schemas.py
@@ -1,5 +1,10 @@
+from enum import Enum
+
 from src.common.schemas import BaseSchema
 
+class AccountTypeEnum(str, Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
 
 class GetCategoryBaseSchema(BaseSchema):
     id: int
@@ -10,6 +15,7 @@ class GetCategoryBaseSchema(BaseSchema):
 class CreateCategorySchema(BaseSchema):
     name: str
     icon_url: str | None = None
+    type: AccountTypeEnum
 
 
 class UpdateCategorySchema(CreateCategorySchema):
